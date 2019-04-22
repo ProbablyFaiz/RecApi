a3c04ff338aa83a567aead28d9ed54a9beac67d2
Faiz <34611522+ProbablyFaiz@users.noreply.github.com>
2019-04-21 18:09:00 -0700

    Update RecContext.cs

--- RecWebApi/Models/RecContext.cs
+++ RecWebApi/Models/RecContext.cs
@@ -38,7 +38,7 @@ namespace RecWebApi.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:recattendance.database.windows.net,1433;Initial Catalog=Rec;Persist Security Info=False;User ID=faiz;Password=Retwe1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=xxx,1433;Initial Catalog=Rec;Persist Security Info=False;User ID=xxx;Password=xxx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
